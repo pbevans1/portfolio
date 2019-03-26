3e70216ed5ee6f8cebc8ec337b3bd5bea4bf2f87
Preston Evans <pbevans1@crimson.ua.edu>
2019-03-26 18:32:24 -0500

    Removed debugging coordinates

--- src/library/displayHelper.c
+++ src/library/displayHelper.c
@@ -366,7 +366,7 @@ string* readString(int starty, int startx, int maxLength, char delimiter) {
 				return NULL;
 			}	
 			if(getmouse(&event) == OK && input->size != 0){
-				printw("x, %d, y: %d. Target: x, %d, y: %d", event.x, event.y, startx, starty);
+				// printw("x, %d, y: %d. Target: x, %d, y: %d", event.x, event.y, startx, starty);
 				if (event.y == starty && event.x >= startx- 5 && event.x <= startx + 5)
 				break;
 			}
